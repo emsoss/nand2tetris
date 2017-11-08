c1c5cc6f740b5697b36465c116eb991860b24375
enabi <emmanuelnabi@gmail.com>
2017-11-07 19:25:21 -0600

    output file added

--- VMTranslator.py
+++ VMTranslator.py
@@ -1,4 +1,6 @@
 
+output_file=open('/home/emmanuel/Desktop/from-nand2tetris/nand2tetris/projects/07/StackArithmetic/SimpleAdd/SimpleAdd.asm', 'w')
+
 def rmv_comments(file):
     '''remove line starting by // and in code commenting'''
     fh=open(file,'r')
@@ -29,36 +31,42 @@ def StackArithmetic(vm_code):
     for line in vm_code:
         if 'push' in line and 'constant' in line:
             #store the value of a constant in the stack pointer and increment the stack pointer
-            asm.append('@SP')
-            asm.append('A=M')
-            asm.append('M='+str(line[-1]))
-            asm.append('@SP')
-            asm.append('M=M+1')
-            print 'pushed'
+            asm.append('@'+str(line[-1])+'\n')
+            asm.append('D=A\n')
+            asm.append('@SP\n')
+            asm.append('A=M\n')
+            asm.append('M=D\n')
+            asm.append('@SP\n')
+            asm.append('M=M+1\n')
 
         elif line== 'add':
             #decrement the stack pointer and store its value in the D register.
-            asm.append('@SP')
-            asm.append('M=M-1')
-            asm.append('A=M')
-            asm.append('D=M')
+            asm.append('@SP\n')
+            asm.append('M=M-1\n')
+            asm.append('A=M\n')
+            asm.append('D=M\n')
             #decrement the stack pointer and complete the addition operation and store the value in the D register
-            asm.append('@SP')
-            asm.append('M=M-1')
-            asm.append('A=M')
-            asm.append('D=D+M')
+            asm.append('@SP\n')
+            asm.append('M=M-1\n')
+            asm.append('A=M\n')
+            asm.append('D=D+M\n')
 
             #store the addition value in the stack pointer Memory
-            asm.append('@SP')
-            asm.append('A=M')
-            asm.append('M=D')
+            asm.append('@SP\n')
+            asm.append('A=M\n')
+            asm.append('M=D\n')
 
             #increment the stack pointer
-            asm.append('@SP')
-            asm.append('M=M+1')
+            asm.append('@SP\n')
+            asm.append('M=M+1\n')
 
+    for line in asm:
+        output_file.write(line)
+    output_file.close()
     return asm
 
+
 test1=StackArithmetic(test)
+print 'lenght', len(test1), test
 for i in test1:
     print i
