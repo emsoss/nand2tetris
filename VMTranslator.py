907c1b7e5abf87c8d40cc0f1984f6e21da30a318
enabi <emmanuelnabi@gmail.com>
2017-11-09 12:59:42 -0600

    solving StackArithmetic boolean for equal

--- VMTranslator.py
+++ VMTranslator.py
@@ -28,6 +28,32 @@ def StackArithmetic(vm_code):
     this function implement the arithmetic operation of a stack machine. it convert the VM Language to Jack assembly code
     '''
     asm=[]
+    count=0
+    if 'True' not in asm:
+        asm.append('@16\n')
+        asm.append('D=A\n')
+        asm.append('@5\n')
+        asm.append('M=D\n')
+
+        asm.append('(True)\n')
+        asm.append('@SP\n')
+        asm.append('A=M\n')
+        asm.append('M=-1\n')
+        asm.append('@5\n')
+        asm.append('A=M\n')
+        asm.append('0;JMP\n')
+        count+=9
+
+    if 'False' not in asm:
+        asm.append('(False)\n')
+        asm.append('@SP\n')
+        asm.append('A=M\n')
+        asm.append('M=0\n')
+        asm.append('@5\n')
+        asm.append('A=M\n')
+        asm.append('0;JMP\n')
+        count+=6
+
     for line in vm_code:
         if 'push' in line and 'constant' in line:
             #store the value of a constant in the stack pointer and increment the stack pointer
@@ -38,7 +64,7 @@ def StackArithmetic(vm_code):
             asm.append('M=D\n')
             asm.append('@SP\n')
             asm.append('M=M+1\n')
-
+            count+=7
         elif line== 'add':
             #decrement the stack pointer and store its value in the D register.
             asm.append('@SP\n')
@@ -59,6 +85,34 @@ def StackArithmetic(vm_code):
             #increment the stack pointer
             asm.append('@SP\n')
             asm.append('M=M+1\n')
+            count+=13
+
+        elif line=='eq':
+
+            #checking if the result=0 ?
+            #decrement the stack pointer and store its value in the D register.
+            asm.append('@SP\n')
+            asm.append('M=M-1\n')
+            asm.append('A=M\n')
+            asm.append('D=M\n')
+            #decrement the stack pointer and complete the addition operation and store the value in the D register
+            asm.append('@SP\n')
+            asm.append('M=M-1\n')
+            asm.append('A=M\n')
+            asm.append('D=D-M\n')
+            count+=8
+            skip=6
+            count+=skip
+            asm.append('@5\n')
+            asm.append('M='+str(count)+'\n')
+            #checking if result=0?
+            asm.append('@True\n')
+            #count+=12
+            asm.append('D;JEQ\n')
+            asm.append('@False\n')
+            asm.append('D,JNE\n')
+
+
 
     for line in asm:
         output_file.write(line)
